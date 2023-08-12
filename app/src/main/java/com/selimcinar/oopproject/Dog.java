b59c0129e3d42d8ceaf036006c0ea0135a263e1e
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2023-08-12 22:32:26 +0300

    Update Dog.java

--- app/src/main/java/com/selimcinar/oopproject/Dog.java
+++ app/src/main/java/com/selimcinar/oopproject/Dog.java
@@ -5,7 +5,7 @@ public class Dog extends Animal {
     public void test (){
         super.sing(); //Diğer classtan miras alınan yapı
     }
-    public  void sing(){ //Bu classta aynı isimde oluşmuş metot
+    public  void sing(){ //Bu classta aynı isimde oluşmuş metot.
         System.out.println("dog class");
     }
 }
